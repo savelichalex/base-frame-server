49358d47f78007f0174e83216525b826a834d537
savelichalex <savelichalex93@gmail.com>
2016-01-07 15:47:25 +0800

    Refactor renderer, try to use static methods

--- Renderer.js
+++ Renderer.js
@@ -1,45 +1,33 @@
-module.exports = function Renderer() {
-};
+'use strict';
 
-Renderer.prototype = {
-    parseTemplate: function (data, obj) {
-        if (!obj || obj.length === 0) {
-            return data;
-        }
-        var template_regexp = /\{\{([\w\d]+)\}\}/g;
+const fs = require('fs');
 
-        data = data + '';
-        data = data.replace(template_regexp, function () {
-            var entry = Array.prototype.slice.call(arguments)[1],
-                val = obj[entry];
-            if (val) {
-                return val;
-            } else {
-                return '';
-            }
-        });
+module.exports =  class Renderer {
+	static parseTemplate(data, obj) {
+		if(!obj || !obj.length) {
+			return data;
+		}
 
-        return data;
-    },
+		const templateRegexp = /\{\{([\w\d]+)\}\}/g;
 
-    render: function (filename, obj, res) {
-        if (typeof filename === 'object') {
-            obj = filename;
-            filename = 'index';
-        } else {
-            filename = filename || 'index';
-        }
-        var path = this.dirname + '/views/' + filename + '.html', //hardcode!!!
-            template,
-            self = this;
-        this._util.fs.readFile(path, function (err, data) {
-            if (err) {
-                res.end();
-            } else {
-                template = self.parseTemplate(data, obj);
-                res.writeHead(200, {'Content-type': 'text/html'});
-                res.end(template);
-            }
-        });
-    }
-};
+		data = data + ''; //data to string conversation
+		return data.replace(tempateRegexp, (_, entry) => obj[entry] ? obj[entry] : '')
+	}
+
+	static parse(file, data, res) {
+		fs.readFile(file, (err, fileData) => {
+			if(err) {
+				res.end();
+			} else {
+				const template = Renderer.parseTemplate(fileData, data);
+				res.writeHead(200, {'Content-type': 'text/html'});
+				res.end(template);
+			}
+		});
+	}
+
+	static json(data, res) {
+		res.writeHead(200, {'Content-type': 'text/json'});
+		res.end(JSON.stringify(obj));
+	}
+}
